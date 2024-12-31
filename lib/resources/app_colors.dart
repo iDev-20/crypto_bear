0ecc91db73be83b10f05bef439171ea738222c3c
Iniunam Idorenyin <iniunamid@gmail.com>
2024-12-31 16:34:08 +0000

    first commit

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primaryColorValue = 0xFF2E2E2E;
+
+//PRIMARY COLORS
+  static const Color white = Color(0xFFFEFEFE);
+  static const Color gradirntBlack = Color(0xFF302F2F);
+  static const Color black = Color(0xFF000000);
+  static const Color paselGradient = Color(0xFF404040);
+
+//SECONDARY COLORS
+  static const Color grey50 = Color(0xFFF5F5F5);
+  static const Color grey200 = Color(0xFFD9D9D9);
+
+//ICON BACKGROUND COLORS
+  static const Color lightOrange = Color(0xFFFFF1AC);
+  static const Color lightBlue = Color(0xFFC4D9D4);
+  static const Color lightPink = Color(0xFFFFD1D1);
+  static const Color orange50 = Color(0xFFFFFDDC);
+  static const Color lightGreen = Color(0xFFDEEEDF);
+  static const Color pink50 = Color(0xFFFBF7F0);
+  static const Color blue100 = Color(0xFFB9CCED);
+  static const Color gree100 = Color(0xFFD6EFDD);
+  static const Color fadedPink = Color(0xFFFFF1AC);
+  static const Color fadedOrange = Color(0xFFFFEFC5);
+
+//TEXT COLORS
+  static const Color grey = Color(0xFFB7B7B7);
+  static const Color grey500 = Color(0xFF888888);
+  static const Color lightRed = Color(0xFFE58080);
+}
---
+++ lib/resources/app_images.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AppImages {
+  AppImages._();
+
+  static SvgPicture svgAdd = SvgPicture.asset('assets/svgs/add.svg');
+  static SvgPicture svgCheck = SvgPicture.asset('assets/svgs/check.svg');
+  static SvgPicture svgClose = SvgPicture.asset('assets/svgs/close.svg');
+  static SvgPicture svgEast = SvgPicture.asset('assets/svgs/east.svg');
+  static SvgPicture svgEmptyCheckBox = SvgPicture.asset('assets/svgs/empty_check_box.svg');
+  static SvgPicture svgFavorite = SvgPicture.asset('assets/svgs/favorite.svg');
+  static SvgPicture svgGrade = SvgPicture.asset('assets/svgs/grade.svg');
+  static SvgPicture svgHome = SvgPicture.asset('assets/svgs/home.svg');
+  static SvgPicture svgInstantMix = SvgPicture.asset('assets/svgs/instant_mix.svg');
+  static SvgPicture svgKeyboardArrowDown = SvgPicture.asset('assets/svgs/keyboard_arrow_down.svg');
+  static SvgPicture svgKeyboardArrowLeft = SvgPicture.asset('assets/svgs/keyboard_arrow_left.svg');
+  static SvgPicture svgKeyboardArrowRight = SvgPicture.asset('assets/svgs/keyboard_arrow_right.svg');
+  static SvgPicture svgKeyboardArrowUp = SvgPicture.asset('assets/svgs/keyboard_arrow_up.svg');
+  static SvgPicture svgLocationOn = SvgPicture.asset('assets/svgs/location_on.svg');
+  static SvgPicture svgLock = SvgPicture.asset('assets/svgs/lock.svg');
+  static SvgPicture svgMail = SvgPicture.asset('assets/svgs/mail.svg');
+  static SvgPicture svgMenu = SvgPicture.asset('assets/svgs/menu.svg');
+  static SvgPicture svgMoreVert = SvgPicture.asset('assets/svgs/more_vert.svg');
+  static SvgPicture svgMyLocationOutlined = SvgPicture.asset('assets/svgs/my_location_outlined.svg');
+  static SvgPicture svgMyLocation = SvgPicture.asset('assets/svgs/my_location.svg');
+  static SvgPicture svgNorthEast = SvgPicture.asset('assets/svgs/north_east.svg');
+  static SvgPicture svgNorthWest = SvgPicture.asset('assets/svgs/north_west.svg');
+  static SvgPicture svgNorth = SvgPicture.asset('assets/svgs/north.svg');
+  static SvgPicture svgNotifications = SvgPicture.asset('assets/svgs/notifications.svg');
+  static SvgPicture svgPerson = SvgPicture.asset('assets/svgs/person.svg');
+  static SvgPicture svgPlayArrow = SvgPicture.asset('assets/svgs/play_arrow.svg');
+  static SvgPicture svgRemove = SvgPicture.asset('assets/svgs/remove.svg');
+  static SvgPicture svgSearchOff = SvgPicture.asset('assets/svgs/search_off.svg');
+  static SvgPicture svgSearchOn = SvgPicture.asset('assets/svgs/search_on.svg');
+  static SvgPicture svgSelectedCheckBox = SvgPicture.asset('assets/svgs/selected_check_box.svg');
+  static SvgPicture svgShoppingCart = SvgPicture.asset('assets/svgs/shopping_cart.svg');
+  static SvgPicture svgSouthEast = SvgPicture.asset('assets/svgs/south_east.svg');
+  static SvgPicture svgSouthWest = SvgPicture.asset('assets/svgs/south_west.svg');
+  static SvgPicture svgSouth = SvgPicture.asset('assets/svgs/south.svg');
+  static SvgPicture svgUnseletedCheckBox = SvgPicture.asset('assets/svgs/unselected_check_box.svg');
+  static SvgPicture svgWest = SvgPicture.asset('assets/svgs/west.svg');
+
+  //addded
+  static SvgPicture svgMailSubtleGrey = SvgPicture.asset('assets/svgs/mail_subtle_grey.svg');
+  static SvgPicture svgLockSubtleGrey = SvgPicture.asset('assets/svgs/lock_subtle_grey.svg');
+  static SvgPicture svgG = SvgPicture.asset('assets/svgs/G.svg');
+  static SvgPicture svgX = SvgPicture.asset('assets/svgs/x.svg');
+  static SvgPicture svgIn = SvgPicture.asset('assets/svgs/In.svg');
+  static SvgPicture svgPersonGrey = SvgPicture.asset('assets/svgs/person_grey.svg');
+  static SvgPicture svgBookmark = SvgPicture.asset('assets/svgs/bookmark.svg');
+  static SvgPicture svgDownload = SvgPicture.asset('assets/svgs/download.svg');
+  static SvgPicture svgSupervisorAccount = SvgPicture.asset('assets/svgs/supervisor_account.svg');
+  static SvgPicture svgArrowLeftAlt = SvgPicture.asset('assets/svgs/arrow_left_alt.svg');
+  static SvgPicture svgSelectedCheckBoxWithBlackTick = SvgPicture.asset('assets/svgs/selected_check_box_with_black_tick.svg');
+  static SvgPicture svgShare = SvgPicture.asset('assets/svgs/share.svg');
+  static SvgPicture svgMore = SvgPicture.asset('assets/svgs/more.svg');
+  static SvgPicture svgBookmarkYellow = SvgPicture.asset('assets/svgs/bookmark_yellow.svg');
+  static SvgPicture svgContentCopy = SvgPicture.asset('assets/svgs/content_copy.svg');
+  static SvgPicture svgCrop = SvgPicture.asset('assets/svgs/crop.svg');
+  static SvgPicture svgInvertColors = SvgPicture.asset('assets/svgs/invert_colors.svg');
+  static SvgPicture svgDynamicFeed = SvgPicture.asset('assets/svgs/dynamic_feed.svg');
+  static SvgPicture svgCrown = SvgPicture.asset('assets/svgs/crown.svg');
+  static SvgPicture svgCropFree = SvgPicture.asset('assets/svgs/crop_free.svg');
+  static SvgPicture svgEdit = SvgPicture.asset('assets/svgs/edit.svg');
+  static SvgPicture svgCrownYellow = SvgPicture.asset('assets/svgs/crown_yellow.svg');
+  static SvgPicture avgAddPhoto = SvgPicture.asset('assets/svgs/add_photo.svg');
+  static SvgPicture svg3User = SvgPicture.asset('assets/svgs/3_user.svg');
+  static SvgPicture svgFilter = SvgPicture.asset('assets/svgs/Filter.svg');
+  static SvgPicture svgSetting = SvgPicture.asset('assets/svgs/Setting.svg');
+}
